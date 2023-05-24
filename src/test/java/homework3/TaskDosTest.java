58d933ca59ba01aa1a994342eb9ceb7a0c42c539
CrKot3 <chalkov02@mail.ru>
2023-05-24 03:14:44 +0300

    Homework 3, commit 1

---
+++ src/test/java/homework3/TaskDosTest.java
@@ -0,0 +1,41 @@
+package homework3;
+
+import homework3.expected.Data;
+import homework3.pages.differnetelementspage.DifferentElementsPage;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class TaskDosTest extends WebDriverSetup {
+    @Test
+    public void testBrowserTitle() {
+        // 2. Assert browser title
+        assertEquals(homePage.getTitle(), Data.SITE_NAME);
+    }
+
+    @Test
+    public void testLogin() {
+        // 4. Assert Username is loggined
+        assertEquals(homePage.getLoggedName(), Data.LOGGED_USER_NAME);
+    }
+
+    @Test
+    public void testElements() {
+        // 5. Open through the header menu Service -> Different Elements Page
+        DifferentElementsPage differentElementsPage = new DifferentElementsPage(driver, homePage);
+
+        // 6. Select checkboxes
+        differentElementsPage.selectCheckbox("Water");
+        differentElementsPage.selectCheckbox("Wind");
+
+        // 7. Select radio
+        differentElementsPage.selectRadio("Selen");
+
+        // 8. Select in dropdown
+        differentElementsPage.selectDropdownOption("Yellow");
+
+        // 9. Assert logs
+        assertEquals(differentElementsPage.getLogs(), Data.DIFFERENT_ELEMENTS_LOGS);
+    }
+}
